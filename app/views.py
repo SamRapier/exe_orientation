523b4f9ef63aa4d441b75b13ee9fc28390c7a34c
Sam Rapier <samrapier@live.co.uk>
2020-02-25 23:04:34 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -67,10 +67,10 @@ def redirect(request):
 
 
 def handler404(request):
-    return render(request, 'app/404.html', status=404)
+    return render(request, '404.html', status=404)
 
 def handler500(request):
-    return render(request, 'app/500.html', status=500)
+    return render(request, '500.html', status=500)
 
 def MVP_treasure_hunt(request):
 	return render(request, 'app/MVP_treasure_hunt.html')
