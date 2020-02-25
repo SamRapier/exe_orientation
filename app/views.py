b7bfe33652eb11e9c71227ad215998d9528de672
Sam Rapier <samrapier@live.co.uk>
2020-02-25 23:44:39 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -19,7 +19,6 @@ def index(request):
 
 def redirect(request):
     global num
-    print("We have arrived")	
     if request.method == 'POST' and 'submit-groupcode' in request.POST:
         print("recieved post and submit-groupcode")
         print(request.POST)
@@ -39,9 +38,10 @@ def redirect(request):
             print("groupcodes matched")
             request.session['groupcode'] = groupcode
             print("session request successful")
-            return render(request, 'app/studentview.html.',{"groupcode":groupcode, "data":info})
+            print(id)
+            return render(request, 'app/studentview.html.',{"groupcode":groupcode, "data":info, "id":id})
 
-            
+
         else:
             print("Wrong")
             messages.error(request, 'The game code does not exist')
@@ -49,19 +49,19 @@ def redirect(request):
     if request.method == 'POST' and 'submit-question' in request.POST:
         groupcode = request.session['groupcode']
         data = str(request.POST.get('answer'))
-        if Questions.objects.filter(answers=data, auto_increment_id=int(num)).exists():
+        if Questions.objects.filter(answers=data, node_num=int(num)).exists():
             num += 1
-            if Questions.objects.filter(auto_increment_id=int(num)).exists():
-             info = Questions.objects.filter(auto_increment_id=num)
+            if Questions.objects.filter(node_num=int(num)).exists():
+             info = Questions.objects.filter(node_num=num)
              messages.success(request, 'Correct!')
              return render(request, 'app/studentview.html.',{"groupcode":groupcode,"data":info,"id":id})
             else:
                 num -=1
-                info = Questions.objects.filter(auto_increment_id=num)
+                info = Questions.objects.filter(node_num=num)
                 messages.success(request, 'You have finished the quiz, well done!')
                 return render(request, 'app/studentview.html.', {"groupcode": groupcode, "data": info, "id": id})
         else:
-            info = Questions.objects.filter(auto_increment_id=num)
+            info = Questions.objects.filter(node_num=num)
             messages.error(request, 'That is the wrong answer, please try again')
             return render(request, 'app/studentview.html.', {"groupcode": groupcode, "data": info, "id": id})
     print(request.method)
