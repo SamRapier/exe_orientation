95ac57d6f2c321ba78ea4762679eabc55d499811
Sam Rapier <samrapier@live.co.uk>
2020-02-25 13:02:33 +0000

    new files

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('404', views.handler404, name='404'),
+	path('500', views.handler500, name='500'),
+	path('MVP_treasure_hunt', views.MVP_treasure_hunt, name='MVP_treasure_hunt'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'app/index.html', context=None)
+
+def handler404(request):
+    return render(request, '404.html', status=404)
+
+def handler500(request):
+    return render(request, '500.html', status=500)
+
+def MVP_treasure_hunt(request):
+	return render(request, 'MVP_treasure_hunt.html')
